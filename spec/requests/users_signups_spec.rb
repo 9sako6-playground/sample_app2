6e0a4cee324b10f5626ec16f1f8b1b1014edb0bb
9sako6 <31821663+9sako6@users.noreply.github.com>
2020-05-27 15:08:02 +0900

    test: signup with account activation

--- spec/requests/users_signups_spec.rb
+++ spec/requests/users_signups_spec.rb
@@ -3,7 +3,9 @@
 require 'rails_helper'
 
 RSpec.describe 'UsersSignupTest', type: :request do
+  include SessionsHelper
   describe 'signup test' do
+    before(:each) { ActionMailer::Base.deliveries.clear }
     it 'reject to signup with invalid information' do
       get signup_path
       expect do
@@ -18,7 +20,7 @@ RSpec.describe 'UsersSignupTest', type: :request do
       end.not_to(change { User.count })
     end
 
-    it 'accept to signup with valid information' do
+    it 'accept to signup with valid information and account activation' do
       get signup_path
       before_count = User.count
       expect do
@@ -31,6 +33,28 @@ RSpec.describe 'UsersSignupTest', type: :request do
           }
         }
       end.to(change { User.count }.from(before_count).to(before_count + 1))
+      expect(ActionMailer::Base.deliveries.size).to eq(1)
+      user = assigns(:user)
+      expect(user.activated?).to eq(false)
+      # 有効化していない状態でログイン
+      get login_path
+      post login_path, params: {
+        session: {
+          email: user.email,
+          password: 'password'
+        }
+      }
+      expect(loggedin?).to eq(false)
+      # 有効化トークンが不正な場合
+      get edit_account_activation_path('invalid token', email: user.email)
+      expect(loggedin?).to eq(false)
+      # メールアドレスが無効な場合
+      get edit_account_activation_path(user.activation_token, email: 'wrong')
+      expect(loggedin?).to eq(false)
+      # 有効なトークンとメールアドレスの場合
+      get edit_account_activation_path(user.activation_token, email: user.email)
+      expect(user.reload.activated?).to eq(true)
+      expect(loggedin?).to eq(true)
     end
   end
 end
