6c7e822259a0d70f175f29fbc5b7d479acccfd69
9sako6 <31821663+9sako6@users.noreply.github.com>
2020-06-01 11:10:39 +0900

    feat: user, micropostsの関連付け

--- spec/models/micropost_spec.rb
+++ spec/models/micropost_spec.rb
@@ -3,5 +3,25 @@
 require 'rails_helper'
 
 RSpec.describe Micropost, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:michael) { create(:michael) }
+  let(:post) { michael.microposts.build(content: 'Lorem ipsum') }
+
+  it 'should be valid' do
+    expect(post.valid?).to eq(true)
+  end
+
+  it 'user id should be present' do
+    post.user_id = nil
+    expect(post.valid?).to eq(false)
+  end
+
+  it 'content should be present' do
+    post.content = '   '
+    expect(post.valid?).to eq(false)
+  end
+
+  it 'content should be at most 140 characters' do
+    post.content = 'a' * 141
+    expect(post.valid?).to eq(false)
+  end
 end
